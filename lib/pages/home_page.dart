ae5806eacdad8089b4a3a7fb120936bf247d575a
MauroCervantes228 <mauroacg228@gmail.com>
2024-02-15 22:33:22 -0600

    ContadorFlutter

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int cont = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Contador"),
+      ),
+      body: Center(
+        child: 
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+          const Text("Número de clicks"),
+          Text("$cont"), //el signo de pesos es para mostrar variables
+          ]
+        )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _crearBotones(),
+    );
+  }
+  
+  Widget _crearBotones() {
+    return Padding(padding: const EdgeInsets.symmetric(horizontal: 10), 
+    child:  Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed:() => _sumar(),
+        ),
+        const SizedBox(width:5),
+        FloatingActionButton(
+          child: const Icon(Icons.remove),
+          onPressed:() => _restar(),
+        ),
+        const SizedBox(width:5),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_zero),
+          onPressed:() => _regresoCero(),
+        ),
+      ],
+    ),
+    );
+  }
+  
+ void _sumar() {
+  setState(() => 
+    cont++
+  );
+ }
+ 
+ void _restar() {
+  setState(() => 
+    cont--
+  );
+  }
+  
+ void _regresoCero() {
+  setState(() => 
+    cont=0
+  );
+  }
+}
